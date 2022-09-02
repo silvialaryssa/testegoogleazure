91d12e7dec68c130a60a32818f286108e4147c04
silvialaryssa <45983373+silvialaryssa@users.noreply.github.com>
2022-09-02 13:12:06 -0300

    Update WebDriverExampleGoogle.java

--- src/weblocalsb/WebDriverExampleGoogle.java
+++ src/weblocalsb/WebDriverExampleGoogle.java
@@ -1,8 +1,13 @@
 package weblocalsb;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 //import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,11 +20,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
  * @author Silvia Branco
  *
  */
+
 public class WebDriverExampleGoogle {
-	        @Test
-		public static void main(String[] args) {
-		WebDriverManager.chromedriver().setup();
-		ChromeDriver driver = new ChromeDriver();
+	@Test 
+	//public static void main(String[] args) {
+	public void exemplPesquisarGoogle(){
+	
+	//	WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.gecko.driver", "C:\\Gecko\\geckodriver.exe");
+			WebDriver driver = new FirefoxDriver();
+		
+		
+		//ChromeDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com");
 	
 		WebElement element1 = driver.findElement(By.name("q"));
