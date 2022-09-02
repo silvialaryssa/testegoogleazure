9953903b3265b432ae53026d3499ef107cef606b
silvialaryssa <45983373+silvialaryssa@users.noreply.github.com>
2022-09-02 13:58:42 -0300

    Update WebDriverExampleGoogle.java

--- src/weblocalsb/WebDriverExampleGoogle.java
+++ src/weblocalsb/WebDriverExampleGoogle.java
@@ -2,12 +2,12 @@ package weblocalsb;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 
 //import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.chrome.ChromeDriver;
